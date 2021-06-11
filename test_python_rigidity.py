2f5fc40489df5efc9b2ed58d00c5c05453291d2d
Your Name <you@example.com>
2021-06-11 12:46:37 +0200

    initial commit

---
+++ test_python_rigidity.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib . pyplot as plt
+import FDApy as fda
+
+# il me faudra un fichier comme cd4 avec tout les rigidités une ligne represente la rigidités en fonction du gap
+# je ne sais pas ou stoker mon argval si j 'ai bien compris cela est mon gap
+
+
+rigidity = pd.read_csv(’a changer .csv’, index col=0)
+
+all_argvals = rigidity.columns.astype(np.int64)
+argvals = {idx: np.array(all_argvals[ ̃np.isnan(row)])
+            for idx, row in enumerate(rigidity.values)}
+values = {idx: row[ ̃np.isnan(row)]
+            for idx, row in enumerate(rigidity.values)}
+rigiditycounts = IrregularFunctionalData({’input dim 0’: argvals}
+
+
+_= plot(cd4counts[5:15])
+plt.xlabel(’Month since seroconversion’)
+plt.ylabel(’CD4 cell counts (log−scale)’)
+plt.title(’CD4 counts for individual 5−14’)
+plt.show()
